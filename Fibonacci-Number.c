52e3fc10141a9855d755c5c10752be9ed7593d86
Aksh Maheshwari <157402019+AkshMaheshwari@users.noreply.github.com>
2024-06-05 12:40:16 +0530

    Create Fibonacci-Number.c

---
+++ Fibonacci-Number.c
@@ -0,0 +1,18 @@
+//Fibonacci number 
+#include <stdio.h>
+#include <stdlib.h>
+int fibonacci(int n)
+{
+  if(n==0)return 0;
+
+  else if (n==1) return 1;
+
+  else return fibonacci(n-1)+fibonacci(n-2);
+}
+int main()
+{
+  int n;
+  printf("Enter a number: ");
+  scanf("%d",&n);
+  printf("fibonacci series upto that number is: %d",fibonacci(n));
+}
