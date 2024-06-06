1b1285fdcf489c7792b633ddd62130a550208add
Aksh Maheshwari <157402019+AkshMaheshwari@users.noreply.github.com>
2024-06-06 09:42:47 +0530

    Create Play-button.c

---
+++ Patterns/Play-button.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int row, col;
+    // Top Half
+    for(row = 1; row <= 4; row++)
+    {
+        for(col = 1; col <= row; col++)
+        {
+            printf("* ");
+        }
+        for(col = 1; col <= 8 - 2 * row; col++)
+        {
+            printf(" ");
+        }
+        printf("\n");
+    }
+    
+    // Bottom Half
+    for(row = 3; row >= 1; row--)
+    {
+        for(col = row; col >= 1; col--)
+        {
+            printf("* ");
+        }
+        for(col = 8 - 2 * row; col >= 1; col--)
+        {
+            printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
